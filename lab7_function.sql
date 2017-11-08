48ea962606e6dd65f5eb4d4b0f4524560cace422
helga-helga <olyalavyagina@gmail.com>
2017-11-08 09:34:01 +0200

    Added function for insertion values in routes
    Insert values if they does not exist already

--- lab7_function.sql
+++ lab7_function.sql
@@ -13,3 +13,16 @@ END;
 $time$ LANGUAGE plpgsql;
 
 SELECT avg_time('12');
+
+-- Create procedure that inserts new route to routes table if it doens'n exist already
+CREATE OR REPLACE FUNCTION insert_route(r_id integer, code varchar(256))
+	RETURNS void AS $$
+BEGIN
+IF NOT EXISTS(SELECT route_id, route_code FROM routes WHERE route_id=r_id OR route_code=code) THEN
+INSERT INTO routes (route_id, route_code) VALUES (r_id, code);
+END IF;
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT insert_route('18', '18');
+SELECT * FROM routes;
